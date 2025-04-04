cf82bf73903c657e6de94821e506e1855dfc9739
Sunita Thapa <sunita@edigitalnepal.edu.np>
2025-04-04 11:30:28 +0545

    fix

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -23,7 +23,7 @@ export const Analytics = () => {
         </section>
         <section className="dropdown-section">
           <span>Weekly</span>
-          <img src={VectorIcon} width={95} height={95} />
+          <img src={VectorIcon} />
         </section>
       </div>
       <div className="chart-wrapper">
